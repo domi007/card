9464a6d0a0be4edb176172c67c05a8670108ff61
= <=>
2018-05-29 14:23:18 +0100

    Initial version, just added some boilerplate

--- card/ICC.py
+++ card/ICC.py
@@ -57,7 +57,7 @@ class ISO7816(object):
     standard file tags available in "file_tags" class dictionnary
     '''
     
-    dbg = 0
+    dbg = 5
     
     INS_dic = {
         0x04 : 'DEACTIVATE FILE',
